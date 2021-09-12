d0c1687390689f9c0592dc9be4a7be2526235271
Gemechis <65822894+Marema-Gemechis@users.noreply.github.com>
2021-09-12 18:14:31 +0300

    Add files via upload

---
+++ friendlist/src/main/java/com/example/friendlist/controller/FriendController.java
@@ -0,0 +1,37 @@
+package com.example.friendlist.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.friendlist.model.Friend;
+
+@Controller
+public class FriendController {
+	// INTRODUCT FRIENDLIST ATTRIBUTE, THAT WILL HOLD LIST OF FRIENDS
+	private List<Friend> friends = new ArrayList<Friend>();
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String index(@RequestParam(name = "friend", required = false) String name, Model model) {
+		friends = new ArrayList<Friend>();
+		Friend friend = new Friend();
+		model.addAttribute("friend", friend);
+		return "index";
+	}
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(@ModelAttribute(name = "friend") Friend friend, Model model) {
+		friends.add(friend);
+		model.addAttribute("friends", friends);
+		// NEXT 2 LINES EMPTY THE NAME FIELD WHEN NEEDED
+		friend = new Friend();
+		model.addAttribute("friend", friend);
+		return "index";
+	}
+}
---
+++ friendlist/src/main/java/com/example/friendlist/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.example.friendlist.controller;
